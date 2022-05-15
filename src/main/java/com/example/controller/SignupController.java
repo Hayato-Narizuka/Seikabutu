ec63b5ae4e1e84449b5001f051c4a37c66fafa26
なり <nari1226@icloud.com>
2022-05-15 14:58:09 +0900

    新規登録画面修正

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,7 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SignupController {
 	
 	/**ユーザー登録画面を表示*/
-	public String getSignup(Model model) {
+	@GetMapping("/signup")
+	public String getSignup() {
 		
 		//ユーザー登録画面に遷移
 		return "user/signup";
